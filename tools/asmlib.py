b1703b9304d0e931a2a52e9626c0895bfb43f7c1
Florent Flament <contact@florentflament.com>
2019-12-07 15:17:45 +0100

    Add tool to dump sprite data from png

--- tools/asmlib.py
+++ tools/asmlib.py
@@ -1,4 +1,6 @@
 def lst2asm(lst):
+    """lst is a list of bytes to dump
+    """
     res = []
     for i,v in enumerate(lst):
         if i%8 == 0:
@@ -9,3 +11,13 @@ def lst2asm(lst):
             res.append(', ')
         res.append("${:02x}".format(v))
     return ''.join(res)
+
+def lbool2int(lst):
+    """Converts a list of boolean to an integer.
+    The list usually contains 8 items for a corresponding an 8 bits integer.
+    """
+    r = 0
+    for b in lst:
+        r <<= 1
+        r |= b
+    return r
---
+++ tools/dump_sprite.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+from sys import argv
+from PIL import Image
+from asmlib import *
+
+def main():
+    sprite_fname = argv[1]
+
+    image = Image.open(sprite_fname)
+    width, height = image.size
+
+    if width != 16:
+        print("Error width expected to be 16, but found {}".format(width))
+        exit(1)
+
+    data = list(image.getdata())
+
+    sprite_l = []
+    sprite_r = []
+    for l in range(height):
+        line_l = []
+        line_r = []
+        for i in range(8):
+            pxl = data[l*width + i]
+            line_l.append( pxl[:3] != (0,0,0) )
+        for i in range(8, 16):
+            pxl = data[l*width + i]
+            line_r.append( pxl[:3] != (0,0,0) )
+        sprite_l.append(lbool2int(line_l))
+        sprite_r.append(lbool2int(line_r))
+
+    print("sprite_l:")
+    print(lst2asm(sprite_l))
+    print("sprite_r:")
+    print(lst2asm(sprite_r))
+
+main()
